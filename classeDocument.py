dee591bb17bfd6d89b6ee1f34a85dd7d3ab7d242
NadiaKaci <nadia.kacin@gmail.com>
2025-11-13 15:25:09 -0500

    update classeDocument.py

--- classeDocument.py
+++ classeDocument.py
@@ -1,11 +1,9 @@
 from abc import ABC, abstractmethod
 
-# Classe abstraite Document
 class Document(ABC):
     def __init__(self, nom, auteur):
         self.nom = nom
         self.auteur = auteur
 
     def __str__(self):
-        return f"[Document  {self.nom}   {self.auteur}]"
-# to confirm
+        return f"[Document: {self.nom} | Auteur: {self.auteur}]"
