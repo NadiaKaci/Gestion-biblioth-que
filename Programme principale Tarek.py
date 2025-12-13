cfa64429592cb3c76709a7d331b51e6feca2a373
Tarek-H <hamrounitarek11@gmail.com>
2025-12-12 20:47:00 -0500

    commit main_console

--- Programme principale Tarek.py
+++
@@ -1,6 +0,0 @@
-# Livres (ISBN, annee)
-class Livre(Document):
-    def __init__(self, nom, auteur, ISBN, annee):
-        super().__init__(nom, auteur)
-        self.ISBN = ISBN
-        self.annee = annee
