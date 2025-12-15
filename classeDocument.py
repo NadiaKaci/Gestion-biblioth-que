167ffe2a67d7aea42b450c70c7689664b946e979
NadiaKaci <nadia.kacin@gmail.com>
2025-12-14 19:50:18 -0500

    Delete classeDocument.py

--- classeDocument.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Document(ABC):
-    def __init__(self, nom, auteur):
-        self.nom = nom
-        self.auteur = auteur
-
-    def __str__(self):
-        return f"[Document: {self.nom} | Auteur: {self.auteur}]"
